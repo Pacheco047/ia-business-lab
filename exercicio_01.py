944d6b4c15503ddc97efea70320352a4bc9fca55
Pacheco047 <lucaspfernandes.12@gmail.com>
2026-09-11 08:02:30 -0300

    feat: adicionar script de interação com o usuário

---
+++ exercicio_01.py
@@ -0,0 +1,7 @@
+nome = input("Digite seu nome: ")
+cidade_cliente = input("Digite sua cidade: ") 
+servico_solicitado = input("Digite o serviço solicitado: ")
+
+print(f"Olá {nome}, você solicitou o serviço de {servico_solicitado} na cidade de {cidade_cliente}.")
+
+#Aqui foram usadas as variaveis nome, cidade_cliente e servico_solicitado, e o input() para armazenar as informações fornecidas pelo usuário. Em seguida, essas variáveis foram utilizadas na função print para exibir uma mensagem personalizada ao usuário.
