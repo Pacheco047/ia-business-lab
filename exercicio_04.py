94470b29574e1f4e25ebaa6f235c748ebc2f7e50
Pacheco047 <lucaspfernandes.12@gmail.com>
2026-09-14 12:48:22 -0300

    feat: adicionar verificação de idade do cliente

---
+++ exercicio_04.py
@@ -0,0 +1,6 @@
+nome_cliente = input("Digite o nome do cliente: ")
+idade_cliente = int(input("Digite a idade do cliente: "))
+if idade_cliente >= 18:
+    print("O cliente é maior de idade.")
+else:
+    print("O cliente é menor de idade.")
